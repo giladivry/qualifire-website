0459e6fb752a68d2d43cc59eddc29c7416a08ae1
Gilad Ivry <gilad.ivry@gmail.com>
2023-07-24 00:20:42 +0300

    terms page

--- components/hero.tsx
+++ components/hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-7xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Keeping LLM apps <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-900 to-red-500">under control</span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-2xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Build AI with confidence - Autonomous Quality Assurance for your generated content, at production, at any scale.</p>
+              <p className="text-2xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Build AI with confidence - Autonomous Quality Assurance for your generated content, in production, at any scale.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-red-900 hover:bg-red-800 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
