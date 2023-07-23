95ae72d6fa1db2f96a3940c77fe2d306d9086902
Gilad Ivry <gilad.ivry@gmail.com>
2023-07-23 20:13:48 +0300

    added gif

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-import VideoThumb from '@/public/images/hero-image.png'
+import VideoThumb from '@/public/images/123.gif'
 import ModalVideo from '@/components/modal-video'
 
 export default function Hero() {
@@ -31,7 +31,7 @@ export default function Hero() {
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-7xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Keeping LLM apps <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">under control</span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+              <p className="text-2xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Build AI with confidence - Autonomous Quality Assurance for your generated content.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
@@ -52,7 +52,6 @@ export default function Hero() {
             video="/videos/video.mp4"
             videoWidth={1920}
             videoHeight={1080} />
-
         </div>
 
       </div>
